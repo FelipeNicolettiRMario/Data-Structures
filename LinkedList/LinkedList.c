bcd8b16df4230458dc9048e91157334ece37c139
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2020-03-07 13:21:35 -0300

    Inserir em lista com cabeça

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -25,14 +25,53 @@ void printListHead(NODE *list){
         printf("%d\n",aux->value);
         aux = aux->sequence;
     }
-    
+
 }
 
+void deleteValueHead(NODE *list){
+    NODE *aux;
+    aux = list->sequence;
+
+    list->sequence = aux->sequence;
+    free(aux);
+
+
+}
+
+void insertValueNoHead(int newValue,NODE *list){
+    NODE * newNode = malloc(sizeof(NODE));
+    newNode->value = newValue;
+    newNode->sequence = list;
+
+    list->sequence = NULL;
+    list = newNode;
+
+}
+
+void printListNoHead(NODE *list){
+    NODE *aux;
+    aux = list;
+
+    while(aux != NULL){
+        printf("%d\n",aux->value);
+        aux = aux->sequence;
+    }
+
+}
+
+void deleteValueNoHead(NODE **list){
+    NODE *aux;
+    aux = *list;
+
+    *list = aux->sequence;
+    free(aux);
+
+}
 
 
 int main(){
     //INICIA LISTA COM CABEÇA
-    NODE *pList; 
+    NODE *pList;
     pList = malloc(sizeof(NODE));
     pList->sequence = NULL;
 
@@ -40,8 +79,22 @@ int main(){
     insertValueHead(7,pList);
 
     insertValueHead(9,pList);
-
+    deleteValueHead(pList);
     printListHead(pList);
+
+    //INICIA LISTA SEM CABEÇA
+    NODE *pListNH;
+    pListNH = malloc(sizeof(NODE));
+    pListNH->value = 2;
+    pListNH->sequence = NULL;
+
+    insertValueNoHead(7,&pListNH);
+    insertValueNoHead(10,&pListNH);
+    insertValueNoHead(10,&pListNH);
+    insertValueNoHead(8,&pListNH);
+    deleteValueNoHead(&pListNH);
+
+    printListNoHead(pListNH);
 
 
 }
