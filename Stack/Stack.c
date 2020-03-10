eaf24bf56c8b968d9bf6a0ff0e9c28f1ab4a7247
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2020-03-10 19:11:48 -0300

    Pilha com função de inserir novo item e printar o top

---
+++ Stack/Stack.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Node{
+    int value;
+    struct Node *sequence;
+    
+}NODE;
+
+void incrementStack(int newValue,NODE **Stack){
+    NODE *newNode,*aux;
+    aux = *Stack;
+
+    newNode = malloc(sizeof(NODE));
+    newNode->sequence = NULL;
+    newNode->value = newValue;
+
+    if(aux != NULL){
+        aux->sequence = newNode;
+        *Stack = aux;
+    }
+    else
+    {
+        *Stack = newNode;
+    }
+    
+}
+
+void printTop(NODE *Stack){
+    NODE *aux;
+    aux = Stack;
+
+    while (aux->sequence != NULL)
+    {
+        aux = aux->sequence;
+    }
+    printf("%d",aux->value);
+
+
+}
+
+int main(){
+    //Iniciando uma pilha
+    NODE *pStack;
+    pStack = malloc(sizeof(NODE));
+    pStack = NULL;
+
+    incrementStack(8,&pStack);
+    incrementStack(9,&pStack);
+
+    printTop(pStack);
+
+}
