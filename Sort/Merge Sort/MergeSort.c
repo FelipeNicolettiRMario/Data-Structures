d6de0294ea76228ba76c563f6e39ac90b36d6d16
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2020-06-21 22:39:23 -0300

    Algoritmo de ordenação merge sort

---
+++ Sort/Merge Sort/MergeSort.c
@@ -0,0 +1,64 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+void merge(int array[], int l, int m, int r) { 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 = r - m; 
+  
+    int L[n1], R[n2]; 
+  
+    for (i = 0; i < n1; i++) 
+        L[i] = array[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = array[m + 1 + j]; 
+  
+    i = 0; 
+    j = 0; 
+    k = l; 
+    while (i < n1 && j < n2) { 
+        if (L[i] <= R[j]) { 
+            array[k] = L[i]; 
+            i++; 
+        } 
+        else { 
+            array[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    while (i < n1) { 
+        array[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    while (j < n2) { 
+        array[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+
+void mergeSort(int array[], int l, int r) { 
+    if (l < r) { 
+
+        int m = l + (r - l) / 2; 
+  
+        mergeSort(array, l, m); 
+        mergeSort(array, m + 1, r); 
+  
+        merge(array, l, m, r); 
+    } 
+} 
+  
+int main() { 
+    int array[] = { 12, 11, 13, 5, 6, 7 }; 
+    int array_size = sizeof(array) / sizeof(array[0]); 
+  
+    mergeSort(array, 0, array_size - 1); 
+
+    return 0; 
+}
